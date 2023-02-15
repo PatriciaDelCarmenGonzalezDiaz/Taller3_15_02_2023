877441d6d9012ef319e96cedb0580b4f91601004
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-15 17:54:01 -0500

    Punto4

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-//Realizar un programa que pueda sumar dos números leídos por teclado 
-//y escribir el resultado
+//Realizar un programa de procesos en el que se almacenen 3 números 
+//en 3 variables A, B y C. El diagrama debe decidir cual es el mayor 
+//y cual es el menor
 
-let num1= Number(prompt("Digite el primer número"));
-let num2= Number(prompt("Digite el segundo número"));
+let A = Number(prompt("Digite el primer número"));
+let B = Number(prompt("Digite el segundo número"));
+let C = Number(prompt("Digite el tercer número"));
+let mayor = 0;
+let menor = 0;
 
-alert(`La suma de ${num1} y ${num2} es ${num1 + num2}`);
+if (A == B && B == C) {
+    alert(`los numeros ${A} , ${B} y ${C2} son iguales`);
+} else {
+    if (A > B) {
+        mayor = A;
+        menor = B;
+    } else {
+        mayor = B;
+        menor = A;
+    }
+    if (mayor < C) {
+        mayor = C;
+    }
+    if (menor > C) {
+        menor = C;
+    }
+    alert(`De los numeros ${A} , ${B} y ${C} , el mayor es ${mayor} y el menor es ${menor}`);
+}
