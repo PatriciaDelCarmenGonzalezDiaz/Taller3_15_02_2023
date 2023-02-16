abbc3516e042151f7715121cf61a43d2efae334c
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-15 19:05:28 -0500

    Punto7

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-//Realizar un programa que lea N números, calcule y escriba 
-//la suma de los pares y el producto de los impares
+//Dados dos números enteros positivos N y D, 
+//se dice que D es un divisor de N si el resto de dividir N entre D es 0. 
+//Se dice que un número N es perfecto si la suma de sus divisores (excluido el propio N) es N
 
-let continuar = true;
+let N = Number(prompt("Digite el primer numero"));
+let D = Number(prompt("Digite el segundo numero"));
 let suma = 0;
-let producto = 1;
-let mensaje = " Los numeros digitados son: ";
-do {
-    let num = Number(prompt("Digite un numero, si desea detenerse Digite una Letra"));
-    if (isNaN(num)) {
-        continuar = false;
-    } else {
-        mensaje += num + ",";
-        if (num % 2 == 0) { //Es Par
-            suma += num;
-        } else { //Es Impar
-            producto *= num;
-        }
+let mensaje = "";
+
+if (N%D == 0) {
+    mensaje = D + " es divisor de " + N;
+} else {
+    mensaje = D + " no es divisor de " + N;
+}
+mensaje += " y la suma de sus divisores ";
+for (var i = 1; i < N; i++) {
+    if (N%i == 0) {
+        suma += i;
+        mensaje += i + "+";
     }
-} while (continuar)
-alert(mensaje + " y la suma de los pares es " + suma + " y el producto de los impares es " + producto);
+}
+// quitamos el ultimo signo mas
+let mensaje2 = mensaje.substring(0,mensaje.length - 1);
+mensaje2 += " = " + suma + ", ";
+
+if (suma == N) {
+    alert(mensaje2 + "El numero es perfecto");
+} else {
+   alert( mensaje2 + "El numero no es perfecto");
+}
